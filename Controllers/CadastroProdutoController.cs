9a614e2d5bdd604edce0b582f6a0338189665e21
julie <julie@JULIEHERVIAS>
2025-04-14 08:53:09 -0300

    Adicionando controlador e view de cadastro de produtos

---
+++ Controllers/CadastroProdutoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EspacoVerde.Controllers
+{
+    public class CadastroProdutoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EspacoVerde.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+    }
+}
